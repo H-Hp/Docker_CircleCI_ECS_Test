e17fd28e071fffd2771170dbba94a77aa8cccd0b
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-20 02:57:02 +0900

    “Contactコントローラー作成/ルーティング追加/アクション追加

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,29 @@
+class ContactsController < ApplicationController
+  def index
+    # 一覧表示の処理
+  end
+
+  def show
+    # 詳細表示の処理
+  end
+
+  def new
+    # 新規作成フォーム表示の処理
+  end
+
+  def create
+    # 新規作成の処理
+  end
+
+  def edit
+    # 編集フォーム表示の処理
+  end
+
+  def update
+    # 更新の処理
+  end
+
+  def destroy
+    # 削除の処理
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   #root 'application#top'
   root 'static_pages#home'
+  resources :contacts
 end
