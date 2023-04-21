e1543e788e5219b98fd0e8667ccd36da4e281e0d
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-21 17:11:37 +0900

    Render.comの環境設定2

--- config/environments/production_render_com.rb
+++ config/environments/production_render_com.rb
@@ -1,4 +1,4 @@
-require_relative "production"#production.rbの設定をオーバーライド
+require_relative "production" #production.rbの設定をオーバーライド
 Rails.application.configure do
   # production.rbを上書きする設定を記述
   config.hosts << "docker-circleci-ecs-test-git.onrender.com"
