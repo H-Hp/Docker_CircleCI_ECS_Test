7c1c36e5a6d1fbd26d9496c01573a4bc37ac1c7d
GitHiroki <yarebadekirusa3333@yahoo.co.jp>
2023-04-24 22:43:06 +0900

    〜完〜

--- config/environments/production-aim-big.rb
+++ config/environments/production-aim-big.rb
@@ -1,11 +1,11 @@
 require_relative "production" #production.rbの設定をオーバーライド
 Rails.application.configure do
   # production.rbを上書きする設定を記述
-  #config.secret_key_base = ENV['SECRET_KEY_BASE']
-  #secret_key_base = ENV['SECRET_KEY_BASE']
-  #SECRET_KEY_BASE = ENV['SECRET_KEY_BASE']
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
+  secret_key_base = ENV['SECRET_KEY_BASE']
+  SECRET_KEY_BASE = ENV['SECRET_KEY_BASE']
 
-  config.secret_key_base = <%= Rails.application.credentials.aim_big[:secret_key_base] %>
-  secret_key_base = <%= Rails.application.credentials.aim_big[:secret_key_base] %>
-  SECRET_KEY_BASE = <%= Rails.application.credentials.aim_big[:secret_key_base] %>
+  #config.secret_key_base = Rails.application.credentials.aim_big[:secret_key_base] 
+  #secret_key_base = Rails.application.credentials.aim_big[:secret_key_base]
+  #SECRET_KEY_BASE = Rails.application.credentials.aim_big[:secret_key_base]
 end
